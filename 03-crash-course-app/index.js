819e6e2052965de040f61367ce84a130594f9701
Jestin James <jestinjames@dhcp-10-77-200-148.elanco.com>
2025-01-29 12:51:23 +0530

    Fix: added update to file read and write

--- 03-crash-course-app/index.js
+++ 03-crash-course-app/index.js
@@ -6,9 +6,8 @@
 //SINGLE IMPORT
 //const generateRandomNumber = require('./utils');
 
+const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
 
-const { generateRandomNumber, celsiusToFahrenheit } = require('./utils')
+console.log(`Random Number ${generateRandomNumber()}`);
 
-console.log (`Random Number ${generateRandomNumber()}`);
-
-console.log(`Celsius to Fahrenheit ${celsiusToFahrenheit(0)}`)
+console.log(`Celsius to Fahrenheit ${celsiusToFahrenheit(0)}`);
--- 03-crash-course-app/server2.js
+++ 03-crash-course-app/server2.js
@@ -17,17 +17,85 @@ const users = [
   },
 ];
 
-const server = createServer((req, res) => {
-  if(req.url === "/api/users" && req.method === "GET") {
-    //res.setHeader("Content-Type", "application/json");
-    //res.write(JSON.stringify(users));
-    //res.end();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
+// Logger middleware api url
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+};
+
+// Route handler for GET /api/users
+const getUsersHandler = (req, res) => {
+  // res.writeHead(200, { "Content-Type": "application/json" });
+  // res.end(JSON.stringify(users));
+
+  res.write(JSON.stringify(users));
+  res.end();
+};
+
+// Route handler for GET /api/users/:id
+const getUserByIdHandler = (req, res) => {
+  const id = req.url.split("/")[3];
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.write(JSON.stringify(user));
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found" }));
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: "User not found" }));
   }
+  res.end();
+};
+
+// Not found handler
+const notFoundHandler = (req, res) => {
+  //   res.writeHead(404, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify({ message: "Route not found" }));
+
+  res.statusCode = 404;
+  res.write(JSON.stringify({ message: "Route not found" }));
+  res.end();
+};
+
+// Create User
+// Route handler for POST /api/users
+const createUserHandler = (req, res) => {
+  let body = "";
+  // Listen for data
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const newUser = JSON.parse(body);
+    users.push(newUser);
+    res.statusCode = 201;
+    res.write(JSON.stringify(newUser));
+    res.end();
+  });
+};
+
+const server = createServer((req, res) => {
+  loggerMiddleware(req, res, () => {
+    jsonMiddleware(req, res, () => {
+      if (req.url === "/api/users" && req.method === "GET") {
+        getUsersHandler(req, res);
+      } else if (
+        req.url.match(/\/api\/users\/([0-9]+)/) &&
+        req.method === "GET"
+      ) {
+        getUserByIdHandler(req, res);
+      } else if (req.url === "/api/users" && req.method === "POST") {
+        createUserHandler(req, res);
+      } else {
+        notFoundHandler(req, res);
+      }
+    });
+  });
 });
 
 server.listen(PORT, () => {
